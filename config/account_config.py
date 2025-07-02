e47cf59814630d7b06b7589fb22229f7834c91dd
TienTung9799 <145518532+TienTung9799@users.noreply.github.com>
2025-07-02 16:50:53 +0700

    Update account_config.py

--- config/account_config.py
+++ config/account_config.py
@@ -2,5 +2,3 @@ class orangehrm_config:
     BASE_URL = "https://opensource-demo.orangehrmlive.com"
     USERNAME = "Admin"
     PASSWORD = "admin123"
-    DEFAULT_TIMEOUT = 10
-    BROWSER = "chrome" 
