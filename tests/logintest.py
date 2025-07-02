672f63833a03a3b7cdff04e50cdbb7b82976f5e8
TienTung9799 <145518532+TienTung9799@users.noreply.github.com>
2025-07-02 18:34:58 +0700

    Update logintest.py file

--- tests/logintest.py
+++ tests/logintest.py
@@ -7,3 +7,4 @@ class TestLogin:
     def test_valid_login(self, driver):
         login_page = LoginPage(driver)
         login_page.login_successfull(orangehrm_config.USERNAME, orangehrm_config.PASSWORD)
+        assert login_page.verify_dashboard_title() is True
